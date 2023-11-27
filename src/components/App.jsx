4fba53504b2b9ae098d26fdee602bc37187a29c3
олена городюк <135028180+OlenaUser1982@users.noreply.github.com>
2023-11-27 12:26:38 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import React from 'react';
+import user from 'data/user.json';
+import Profile from './Profile/Profile';
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
+        padding: '30px 60px',
         display: 'flex',
-        justifyContent: 'center',
+        flexDirection: 'column',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        fontSize: 25,
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
